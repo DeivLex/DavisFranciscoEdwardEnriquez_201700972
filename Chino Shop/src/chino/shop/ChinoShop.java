f198bc52c29d12ee74abec1cf03f3ae82c3d4e75
DeivLex <peloliso.98@gmail.com>
2019-11-02 20:00:51 -0600

    Avances

---
+++ Chino Shop/src/chino/shop/ChinoShop.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chino.shop;
+
+/**
+ *
+ * @author Davis
+ */
+public class ChinoShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Grafica ventana = new Grafica();
+        ventana.setTitle("Inicio De Sesion");
+        ventana.show();
+        // TODO code application logic here
+    }
+    
+}
